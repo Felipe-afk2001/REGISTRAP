4a5de5cbbacb25536a6d83a7060891fdbded932d
felipe-afk2001 <fe.medinar@duocuc.cl>
2021-09-28 21:17:39 -0300

    AVANCE INICIAL

---
+++ testeo/src/app/login/login.page.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  correo: string;
+  contrasena: string;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
